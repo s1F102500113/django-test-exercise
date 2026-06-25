efd79b165762e0b96c79297e0c421d2654ce52bd
1F10250011 <s1F102500113@iniad.org>
2026-06-25 14:12:04 +0900

    詳細ページを追加

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
@@ -8,9 +9,11 @@ from todo.models import Task
 
 def index(request):
     if request.method == 'POST':
+        due = parse_datetime(request.POST['due_at']) if request.POST['due_at'] else None
+
         task = Task(
             title=request.POST['title'],
-            due_at=make_aware(parse_datetime(request.POST['due_at']))
+            due_at=make_aware(due) if due else None
         )
         task.save()
 
@@ -25,3 +28,16 @@ def index(request):
         'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
+
+
+def detail(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+
+    context = {
+        'task': task,
+    }
+    return render(request, 'todo/detail.html', context)
+
